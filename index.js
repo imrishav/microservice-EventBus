65096922db753acc452a50cecbe3286c6a046bcf
Rishav Sinha <rishav@live.co.uk>
2020-07-29 23:08:09 +0530

    Dockerized implemented k8s Services & deployment

--- index.js
+++ index.js
@@ -8,14 +8,23 @@ app.use(bodyParse.json());
 
 app.use(cors());
 
+const events = [];
+
 app.post('/events', (req, res) => {
   const event = req.body;
 
-  axios.post('http://localhost:4000/events', event);
-  axios.post('http://localhost:4001/events', event);
-  axios.post('http://localhost:4002/events', event);
+  events.push(event);
+
+  axios.post('http://post-clusterip-srv:4000/events', event);
+  axios.post('http://comments-srv:4001/events', event);
+  axios.post('http://query-srv:4002/events', event);
+  axios.post('http://moderation-srv:4003/events', event);
 
   res.status(200).send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+  res.send(events);
 });
 
 app.listen('4005', () => {
